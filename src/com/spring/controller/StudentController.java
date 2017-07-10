494cd9ec39e971295ac2b97ad1e6694adb919a69
argho2612 <argho.chakr@gmail.com>
2017-07-10 20:21:30 +0530

    Q4

--- src/com/spring/controller/StudentController.java
+++ src/com/spring/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,5 +12,11 @@ public class StudentController {
     ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
         return modelAndView;
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello.html")
+    String hello() {
+        return "Hello world";
     }
 }
