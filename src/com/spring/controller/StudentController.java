6e9e65e223904d4cd0eb6332737200aa75de225f
argho2612 <argho.chakr@gmail.com>
2017-07-10 21:54:22 +0530

    Q7

--- src/com/spring/controller/StudentController.java
+++ src/com/spring/controller/StudentController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Map;
 
 @Controller
-//@RequestMapping("/dummy")
 public class StudentController {
 
     @RequestMapping("/hello.html/{fname}/{lname}")
     @ResponseBody
-    String returnName(@PathVariable("fname") String firstName, @PathVariable("lname") String lastName) {
-        return firstName+" "+lastName;
+    String returnNameMap(@PathVariable Map<String, String> requestMap) {
+        return requestMap.get("fname") + " " + requestMap.get("lname");
     }
+
+//    @RequestMapping("/hello.html/{fname}/{lname}")
+//    @ResponseBody
+//    String returnName(@PathVariable("fname") String firstName, @PathVariable("lname") String lastName) {
+//        return firstName+" "+lastName;
+//    }
 
 //    @ResponseBody
 //    @RequestMapping("/index")
