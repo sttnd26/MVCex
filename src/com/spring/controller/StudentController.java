dad844daf0186817a336e15922d04c2e93be6150
argho2612 <argho.chakr@gmail.com>
2017-07-10 20:57:17 +0530

    Q6

--- src/com/spring/controller/StudentController.java
+++ src/com/spring/controller/StudentController.java
@@ -1,24 +1,32 @@
 package com.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import java.util.Map;
 
 @Controller
+//@RequestMapping("/dummy")
 public class StudentController {
 
+    @RequestMapping("/hello.html/{fname}/{lname}")
     @ResponseBody
-    @RequestMapping("/index")
-    ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("msg", "Hello World from jsp");
-        return modelAndView;
+    String returnName(@PathVariable("fname") String firstName, @PathVariable("lname") String lastName) {
+        return firstName+" "+lastName;
     }
 
+//    @ResponseBody
+//    @RequestMapping("/index")
+//    ModelAndView index() {
+//        ModelAndView modelAndView = new ModelAndView("index");
+//        modelAndView.addObject("msg", "Hello World from jsp");
+//        return modelAndView;
+//    }
     @ResponseBody
     @RequestMapping("/hello.html")
     String hello() {
         return "Hello world";
     }
+
 }
