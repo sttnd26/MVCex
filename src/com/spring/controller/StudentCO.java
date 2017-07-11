0cff0de3770fac1fa789aff94e358ad4647ee414
sttnd26 <snigdharud@gmail.com>
2017-07-11 08:15:05 +0530

    Add files via upload

---
+++ src/com/spring/controller/StudentCO.java
@@ -0,0 +1,22 @@
+package com.spring.controller;
+
+public class StudentCO {
+    String firstname;
+    String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
